781bc04e494ba56efd6545513f669fbb5b7763f3
Alexander Belov <alexander.belov@zyfra.com>
2024-02-16 02:39:36 +0300

    fix sgroups

---
+++ terraform/cplane_sgroup.tf
@@ -0,0 +1,34 @@
+resource "yandex_vpc_security_group" "control_plane_sg" {
+  name       = "control-plane-sg"
+  network_id = yandex_vpc_network.k8s_network.id
+  description = "Security group for Kubernetes control plane nodes"
+}
+
+resource "yandex_vpc_security_group_rule" "control_plane_api_server" {
+  security_group_binding = yandex_vpc_security_group.control_plane_sg.id
+  direction              = "ingress"
+  description            = "Kubernetes API Server"
+  protocol               = "tcp"
+  port                   = 6443
+  v4_cidr_blocks         = ["0.0.0.0/0"]
+}
+
+resource "yandex_vpc_security_group_rule" "control_plane_etcd_api" {
+  security_group_binding = yandex_vpc_security_group.control_plane_sg.id
+  direction              = "ingress"
+  description            = "etcd server client API"
+  protocol               = "tcp"
+  from_port              = 2379
+  to_port                = 2380
+  v4_cidr_blocks         = ["0.0.0.0/0"]
+}
+
+resource "yandex_vpc_security_group_rule" "control_plane_kubelet_api" {
+  security_group_binding = yandex_vpc_security_group.control_plane_sg.id
+  direction              = "ingress"
+  description            = "Kubelet API"
+  protocol               = "tcp"
+  from_port              = 10250
+  to_port                = 10250
+  v4_cidr_blocks         = ["0.0.0.0/0"]
+}
--- terraform/wnode_sgroups.tf
+++ terraform/wnode_sgroups.tf
@@ -1,36 +1,35 @@
 resource "yandex_vpc_security_group" "worker_node_sg" {
-  name        = "worker-node-sg"
-  network_id  = yandex_vpc_network.k8s_network.id
+  name       = "worker-node-sg"
+  network_id = yandex_vpc_network.k8s_network.id
   description = "Security group for Kubernetes worker nodes"
 }
 
 resource "yandex_vpc_security_group_rule" "worker_kubelet_api" {
-  security_group_id = yandex_vpc_security_group.worker_node_sg.id
-  description       = "Kubelet API"
-  direction         = "INGRESS"
-  protocol          = "tcp"
-  from_port         = 10250
-  to_port           = 10250
-  v4_cidr_blocks    = ["0.0.0.0/0"]
+  security_group_binding = yandex_vpc_security_group.worker_node_sg.id
+  direction              = "ingress"
+  description            = "Kubelet API"
+  protocol               = "tcp"
+  from_port              = 10250
+  to_port                = 10250
+  v4_cidr_blocks         = ["0.0.0.0/0"]
 }
 
 resource "yandex_vpc_security_group_rule" "nodeport_services_tcp" {
-  security_group_id = yandex_vpc_security_group.worker_node_sg.id
-  description       = "NodePort Services TCP"
-  direction         = "INGRESS"
-  protocol          = "tcp"
-  from_port         = 30000
-  to_port           = 32767
-  v4_cidr_blocks    = ["0.0.0.0/0"]
+  security_group_binding = yandex_vpc_security_group.worker_node_sg.id
+  direction              = "ingress"
+  description            = "NodePort Services TCP"
+  protocol               = "tcp"
+  from_port              = 30000
+  to_port                = 32767
+  v4_cidr_blocks         = ["0.0.0.0/0"]
 }
 
 resource "yandex_vpc_security_group_rule" "nodeport_services_udp" {
-  security_group_id = yandex_vpc_security_group.worker_node_sg.id
-  description       = "NodePort Services UDP"
-  direction         = "INGRESS"
-  protocol          = "udp"
-  from_port         = 30000
-  to_port           = 32767
-  v4_cidr_blocks    = ["0.0.0.0/0"]
+  security_group_binding = yandex_vpc_security_group.worker_node_sg.id
+  direction              = "ingress"
+  description            = "NodePort Services UDP"
+  protocol               = "udp"
+  from_port              = 30000
+  to_port                = 32767
+  v4_cidr_blocks         = ["0.0.0.0/0"]
 }
-
